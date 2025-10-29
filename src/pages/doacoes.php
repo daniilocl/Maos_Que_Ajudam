bd18303390adc5010d731162d579365a81e037c2
daniilocl <danilocarvalho.matos67@gmail.com>
2025-10-28 23:32:34 -0300

    chore(structure): alterando organização de pastas

---
+++ src/pages/doacoes.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+    <title>Projetos</title>
+</head>
+<body>
+    <?php include '../../src/components/header.php'; ?>
+
+    <main class="container mt-4">
+        <!-- Conteúdo da página -->
+    </main>
+
+    <?php include '../../src/components/footer.php'; ?>
+
+    <!-- Scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
