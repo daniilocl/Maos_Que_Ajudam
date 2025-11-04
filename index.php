43bc8c61dcb616a2f08ad1a40f6677b5caf1f1ce
daniilocl <danilocarvalho.matos67@gmail.com>
2025-11-04 01:01:02 -0300

    feat: adicionando gradient na navbar e adc botão do whatsapp fixo

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
   <title>Mãos que Ajudam</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="public/css/home.css">
-  <link rel="stylesheet" href="public/css/style.css">
+  <link rel="stylesheet" href="./public/css/home.css">
+  <link rel="stylesheet" href="./public/css/style.css">
 </head>
 
 <body>
@@ -100,6 +100,7 @@
   <!-- Footer -->
   <?php include 'src/components/footer.php'; ?>
 
+  
   <!-- Scripts -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
