692bb740cfe20da2749d633b8e0c341e3c3f8dd0
daniilocl <danilocarvalho.matos67@gmail.com>
2025-11-11 00:48:39 -0300

    fix: refatorando alguns códigos

--- src/components/header.php
+++ src/components/header.php
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
   <div class="container-fluid">
     <a class="navbar-brand d-flex align-items-center" href="/projeto_ong/index.php">
-      <img src="/projeto_ong/public/imagens/logo/Logo_MaosQueAjudam.jpg" 
+      <img src="/Maos_Que_Ajudam/public/imagens/logo/Logo_MaosQueAjudam.jpg" 
            alt="Logo Mãos que Ajudam" 
            width="50" height="50" 
            class="me-2 rounded-circle">
@@ -16,21 +16,21 @@
 
     <div class="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
       <ul class="navbar-nav">
-        <li class="nav-item"><a class="nav-link active" href="/projeto_ong/index.php">Home</a></li>
-        <li class="nav-item"><a class="nav-link" href="/projeto_ong/src/views/doacoes.php">Doações</a></li>
+        <li class="nav-item"><a class="nav-link active" href="/Maos_Que_Ajudam/index.php">Home</a></li>
+        <li class="nav-item"><a class="nav-link" href="/Maos_Que_Ajudam/src/views/doacoes.php">Doações</a></li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Mais</a>
           <ul class="dropdown-menu dropdown-menu-end">
-            <li><a class="dropdown-item" href="/projeto_ong/src/views/projetos.php">Projetos</a></li>
-            <li><a class="dropdown-item" href="/projeto_ong/src/views/contribuicoes.php">Contribuições e Parcerias</a></li>
-            <li><a class="dropdown-item" href="/projeto_ong/src/views/cadastro_voluntario.php">Cadastro de Voluntário</a></li>
-            <li><a class="dropdown-item" href="/projeto_ong/src/views/administracao.php">Administração</a></li>
+            <li><a class="dropdown-item" href="/Maos_Que_Ajudam/src/views/projetos.php">Projetos</a></li>
+            <li><a class="dropdown-item" href="/Maos_Que_Ajudam/src/views/contribuicoes.php">Contribuições e Parcerias</a></li>
+            <li><a class="dropdown-item" href="/Maos_Que_Ajudam/src/views/cadastro_voluntario.php">Cadastro de Voluntário</a></li>
+            <li><a class="dropdown-item" href="/Maos_Que_Ajudam/src/views/administracao.php">Administração</a></li>
           </ul>
         </li>
       </ul>
     </div>
 
-    <a href="/projeto_ong/src/views/login/login.php" class="btn btn-light text-primary ms-lg-3 mt-2 mt-lg-0 btn-login">Login</a>
+    <a href="/Maos_Que_Ajudam/src/views/login/login.php" class="btn btn-light text-primary ms-lg-3 mt-2 mt-lg-0 btn-login">Login</a>
   </div>
 </nav>
 
