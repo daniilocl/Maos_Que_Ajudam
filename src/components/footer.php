9f34bd016d6239a9320d8d525362e53d8d6abcd5
Tuta011 <silasruas8@gmail.com>
2025-11-21 22:55:05 -0300

    Resolvendo bug de botão duplicado no header

--- src/components/footer.php
+++ src/components/footer.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-
-<body>
 
   <!-- HERO SECTION -->
   <style>
@@ -118,7 +108,3 @@
 
   <!-- Scripts: carregar Bootstrap JS uma vez -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-
-</body>
-
-</html>
